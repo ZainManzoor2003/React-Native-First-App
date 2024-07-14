88ee806d7e5437d2873574b96cfa6d3802a1a51e
ZainManzoor2003 <101002312+ZainManzoor2003@users.noreply.github.com>
2024-07-14 15:36:05 +0500

    Update controller.js

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -24,8 +24,8 @@ const connection = (req, res) => {
 const login = async (req, res) => {
     let { email, password } = req.body
     try {
-        const user = await UserModel.findOne({ email: email })
         res.send({email:email})
+        const user = await UserModel.findOne({ email: email })
         return;
         if (user) {
             if (user.password == password) {
